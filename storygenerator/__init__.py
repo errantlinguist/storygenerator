30c86326d527341843d05ce3d1e9a56970026069
errantlinguist <errantlinguist+github@gmail.com>
2018-03-30 17:31:35 -0400

    de-coupled prologue/epilogue name parsing from class "Chapter"

--- storygenerator/__init__.py
+++ storygenerator/__init__.py
@@ -7,7 +7,7 @@ __copyright__ = "Copyright (C) 2018 Todd Shore"
 __license__ = "Apache License, Version 2.0"
 
 import re
-from typing import Optional, Sequence, Tuple, Union
+from typing import Optional, Sequence
 
 __DIGITS_PATTERN = re.compile('(\d+)')
 
@@ -40,34 +40,3 @@ class Chapter(object):
 		fields = ("{seq=", str(self.seq), ", title=", str(self.title), ", pars=", str(self.pars), "}")
 		field_repr = "".join(fields)
 		return self.__class__.__name__ + field_repr
-
-	@property
-	def seq_sort_key(self) -> Tuple[int, Tuple[Union[int, str], ...]]:
-		return chapter_seq_sort_key(self.seq)
-
-
-def chapter_seq_sort_key(seq: str) -> Tuple[int, Tuple[Union[int, str], ...]]:
-	if seq.lower() == "prologue":
-		group = -1
-	elif seq.lower() == "epilogue":
-		group = 1
-	else:
-		group = 0
-	return group, natural_keys(seq)
-
-
-def natural_keys(text: str) -> Tuple[Union[int, str], ...]:
-	"""
-	alist.sort(key=natural_keys) sorts in human order
-
-	:see: http://nedbatchelder.com/blog/200712/human_sorting.html
-	:see: http://stackoverflow.com/a/5967539/1391325
-	"""
-	return tuple(__atoi(c) for c in __DIGITS_PATTERN.split(text))
-
-
-def __atoi(text: str) -> Union[int, str]:
-	"""
-	:see: http://stackoverflow.com/a/5967539/1391325
-	"""
-	return int(text) if text.isdigit() else text
