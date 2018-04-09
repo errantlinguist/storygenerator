2f210eda382b399d2c3db1057e3cc0ebfd7ad305
errantlinguist <errantlinguist+github@gmail.com>
2018-04-09 00:12:45 -0400

    now splitting sequence files into batches

--- create_sequences.py
+++ create_sequences.py
@@ -9,6 +9,7 @@ __copyright__ = "Copyright (C) 2018 Todd Shore"
 __license__ = "Apache License, Version 2.0"
 
 import argparse
+import math
 import os
 from typing import Iterable, List, Tuple
 
@@ -24,14 +25,41 @@ class NPZSequenceWriter(object):
 	OUTPUT_FILE_DELIMITER = '\t'
 	OUTPUT_FILE_EXTENSION = ".npz"
 
-	def __init__(self, outdir: str):
+	def __init__(self, outdir: str, batch_size: int):
 		self.outdir = outdir
+		self.batch_size = float(batch_size)
 
 	def __call__(self, infile: str, x: np.array, y: np.array):
+		# Don't touch this: The shape of the two arrays is likely different in dimensionality
+		assert x.shape[0] == y.shape[0]
+		assert x.shape[-1] == y.shape[-1]
+
 		infile_name_base = os.path.splitext(os.path.basename(infile))[0]
-		outpath = os.path.join(self.outdir, infile_name_base + self.OUTPUT_FILE_EXTENSION)
-		print("Writing features extracted from \"{}\" to \"{}\".".format(infile, outpath))
-		np.savez_compressed(outpath, x=x, y=y)
+
+		total_size = x.nbytes + y.nbytes
+		# print("Total size in MB: {}".format(total_size / 1024 / 1024))
+		batch_count = math.ceil(total_size / self.batch_size)
+		print("Splitting data from \"{}\" into {} batch(es).".format(infile, batch_count))
+		rows_per_batch = math.ceil(x.shape[0] / batch_count)
+		remaining_row_count = x.shape[0]
+		next_batch_start_idx = 0
+		next_batch_id = 1
+		while remaining_row_count > 0:
+			next_batch_size = min(rows_per_batch, remaining_row_count)
+			next_batch_end_idx = next_batch_start_idx + next_batch_size
+			batch_x = x[next_batch_start_idx: next_batch_end_idx]
+			batch_y = y[next_batch_start_idx: next_batch_end_idx]
+
+			outpath = os.path.join(self.outdir,
+								   infile_name_base + "-" + str(next_batch_id) + self.OUTPUT_FILE_EXTENSION)
+			print(
+				"Writing batch {} of features extracted from \"{}\" to \"{}\" (start idx: {}; end idx: {}; batch rows: {}).".format(
+					next_batch_id, infile, outpath, next_batch_start_idx, next_batch_end_idx, next_batch_size))
+			np.savez_compressed(outpath, x=batch_x, y=batch_y)
+
+			remaining_row_count -= next_batch_size
+			next_batch_start_idx = next_batch_end_idx
+			next_batch_id += 1
 
 
 def create_sequences(features: np.array, maxlen: int, sampling_rate: int) -> Tuple[
@@ -79,6 +107,8 @@ def __create_argparser() -> argparse.ArgumentParser:
 						help="The maximum sequence length to create.")
 	result.add_argument("-s", "--sampling-rate", dest="sampling_rate", metavar="RATE", type=int, default=3,
 						help="The sequence sampling rate to use.")
+	result.add_argument("-m", "--memory", metavar="MBYTES", type=int, default=512,
+						help="The maximum size of a sequence training batch in megabytes.")
 	return result
 
 
@@ -89,7 +119,8 @@ def __main(args):
 	print("Will save sequence data to \"{}\".".format(outdir))
 	maxlen = args.max_length
 	sampling_rate = args.sampling_rate
-	print("Maximum length: {}; Sampling rate: {}".format(maxlen, sampling_rate))
+	batch_size = args.memory
+	print("Maximum length: {}; Sampling rate: {}; Batch size (in MB): {}".format(maxlen, sampling_rate, batch_size))
 
 	file_walker = NPZFileWalker()
 	feature_dir = os.path.join(indir, OUTPUT_FEATURE_DIRNAME)
@@ -99,10 +130,10 @@ def __main(args):
 	seq_outdir = os.path.join(outdir, OUTPUT_SEQUENCE_DIRNAME)
 	os.makedirs(seq_outdir, exist_ok=True)
 	print("Writing sequence data to \"{}\".".format(seq_outdir))
-	writer = NPZSequenceWriter(seq_outdir)
+	writer = NPZSequenceWriter(seq_outdir, batch_size * 1024 * 1024)
 	for infile in infiles:
 		x, y = read_file(infile, maxlen, sampling_rate)
-		writer(infile, x, y)
+		writer(infile, np.asarray(x), np.asarray(y))
 
 
 if __name__ == "__main__":
